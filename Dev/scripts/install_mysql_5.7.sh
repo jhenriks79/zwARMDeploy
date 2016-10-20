d55e09303e5a1cb1f27d6fa1b9b5f59025eed8ba
Justin Henriksen <justhe@microsoft.com>
2016-10-20 15:41:44 -0700

    - Commented out post install script.

--- Dev/scripts/install_mysql_5.7.sh
+++ Dev/scripts/install_mysql_5.7.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-mysqlPassword=$1
-sudo apt-get update
+#mysqlPassword=$1
+#sudo apt-get update
 #no password prompt while installing mysql server
 #export DEBIAN_FRONTEND=noninteractive
 
 #another way of installing mysql server in a Non-Interactive mode
-echo "mysql-server-5.7 mysql-server/root_password password $mysqlPassword" | sudo debconf-set-selections 
-echo "mysql-server-5.7 mysql-server/root_password_again password $mysqlPassword" | sudo debconf-set-selections 
+#echo "mysql-server-5.7 mysql-server/root_password password $mysqlPassword" | sudo debconf-set-selections 
+#echo "mysql-server-5.7 mysql-server/root_password_again password $mysqlPassword" | sudo debconf-set-selections 
 
 #install mysql-server 5.7
-sudo apt-get -y install mysql-server-5.7
+#sudo apt-get -y install mysql-server-5.7
 
 #set the password
-sudo mysqladmin -u root password "$mysqlPassword"   #without -p means here the initial password is empty
+#sudo mysqladmin -u root password "$mysqlPassword"   #without -p means here the initial password is empty
 
 #alternative update mysql root password method
 #sudo mysql -u root -e "set password for 'root'@'localhost' = PASSWORD('$mysqlPassword')"
